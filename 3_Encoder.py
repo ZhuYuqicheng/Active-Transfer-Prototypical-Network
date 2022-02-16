1a4e52504beb12d2bfb7efccd92bc1a1f26e5e47
Yuqicheng Zhu <yuqicheng.zhu@tum.de>
2022-02-16 02:24:03 +0100

    prototypical network implemented

--- 3_Encoder.py
+++ 3_Encoder.py
@@ -2,6 +2,8 @@
 import random
 from datetime import datetime
 import os
+import copy
+from tkinter import Y
 import numpy as np
 import pandas as pd
 from sklearn.neighbors import KernelDensity
@@ -13,8 +15,10 @@ from tensorflow.python.keras.layers import Flatten
 from tensorflow.python.keras.layers import Dropout
 from tensorflow.python.keras.layers import Conv1D
 from tensorflow.python.keras.layers import MaxPooling1D
+from tensorflow.python.keras.models import Model
 from tensorflow import set_random_seed
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
 
 from importlib import reload
@@ -64,6 +68,28 @@ def load_dataset_group(group):
 	y = load_file('./UCI HAR Dataset/' + group + '/y_'+group+'.txt')
 	return X, y
 
+def scale_data(trainX, testX):
+	# remove overlap
+	cut = int(trainX.shape[1] / 2)
+	longX = trainX[:, -cut:, :]
+	# flatten windows
+	longX = longX.reshape((longX.shape[0] * longX.shape[1], longX.shape[2]))
+	# flatten train and test
+	flatTrainX = trainX.reshape((trainX.shape[0] * trainX.shape[1], trainX.shape[2]))
+	flatTestX = testX.reshape((testX.shape[0] * testX.shape[1], testX.shape[2]))
+	# standardize
+	s = StandardScaler()
+	# fit on training data
+	s.fit(longX)
+	# apply to training and test data
+	longX = s.transform(longX)
+	flatTrainX = s.transform(flatTrainX)
+	flatTestX = s.transform(flatTestX)
+	# reshape
+	flatTrainX = flatTrainX.reshape((trainX.shape))
+	flatTestX = flatTestX.reshape((testX.shape))
+	return flatTrainX, flatTestX
+
 # load the dataset, returns train and test X and y elements
 def load_dataset():
 	# load all train
@@ -76,7 +102,9 @@ def load_dataset():
 	# one hot encode y
 	trainy = tf.keras.utils.to_categorical(trainy)
 	testy = tf.keras.utils.to_categorical(testy)
-	result = {"trainX": trainX, "trainy": trainy, "testX": testX, "testy": testy}
+	# standardization
+	flatTrainX, flatTestX = scale_data(trainX, testX)
+	result = {"trainX": flatTrainX, "trainy": trainy, "testX": flatTestX, "testy": testy}
 	return result
 
 # %% Pre-trained Encoder class
@@ -88,16 +116,12 @@ class Encoder:
 		self.testy = data["testy"]
 
 	def train_model(self, verbose=0, epochs=10, batch_size=32, \
-		filters=32, kernel=7, feature_num=100, \
-		standardize=True, plot_acc=False):
+		filters=32, kernel=7, feature_num=100, plot_acc=False):
 		"""pre-training process of the PN Encoder"""
 		# get dimension
 		n_timesteps =  self.trainX.shape[1]
 		n_features = self.trainX.shape[2]
 		n_outputs = self.trainy.shape[1]
-		# standardization
-		if standardize:
-			self.scale_data()
 		# define model structure
 		model = Sequential()
 		model.add(Conv1D(filters=filters, kernel_size=kernel, activation='relu', input_shape=(n_timesteps,n_features)))
@@ -130,49 +154,80 @@ class Encoder:
 		plt.ylabel("accuracy")
 		plt.title("Learning Curve of Pre-trained Encoder")
 
-	def scale_data(self):
-		"""standardization for HAR data only"""
-		# remove overlap
-		cut = int(self.trainX.shape[1] / 2)
-		longX = self.trainX[:, -cut:, :]
-		# flatten windows
-		longX = longX.reshape((longX.shape[0] * longX.shape[1], longX.shape[2]))
-		# flatten train and test
-		flatTrainX = self.trainX.reshape((self.trainX.shape[0] * self.trainX.shape[1], self.trainX.shape[2]))
-		flatTestX = self.testX.reshape((self.testX.shape[0] * self.testX.shape[1], self.testX.shape[2]))
-		# standardize
-		s = StandardScaler()
-		# fit on training data
-		s.fit(longX)
-		# apply to training and test data
-		longX = s.transform(longX)
-		flatTrainX = s.transform(flatTrainX)
-		flatTestX = s.transform(flatTestX)
-		# reshape
-		flatTrainX = flatTrainX.reshape((self.trainX.shape))
-		flatTestX = flatTestX.reshape((self.testX.shape))
-		# output
-		self.trainX = flatTrainX
-		self.testX = flatTestX
-
 # %% Prototypical Network Class
-class PrototypicalNetwork:
-	def __init__(self, model_path):
+class ActiveLearning:
+	def __init__(self, model_path, data):
 		# load the pre-trained encoder
-		self.model = keras.models.load_model(model_path)
+		base_model = keras.models.load_model(model_path)
+		# feature extraction
+		model = Model(inputs=base_model.input, outputs=base_model.get_layer("feature").output)
+		self.features = model.predict(data["X"])
+		# initialize pool
+		self.X_pool = copy.deepcopy(self.features)
+		self.y = data["y"]
+		self.y_pool = copy.deepcopy(self.y)
+
+	def random_selection(self, X_pool):
+		n_samples = len(X_pool)
+		query_idx = np.random.choice(range(n_samples), len(np.unique(self.y)))
+		return query_idx
 	
-	#def feature_extraction(self, data):
+	def pn_prediction(self, prototyps, x):
+		dist = np.sum((np.array(prototyps)-x)**2, axis=1)
+		idx = np.argmin(dist)
+		pred = prototyps.index[idx]
+		return pred
+
+	def run_prototypical_network(self, support_X, support_y):
+		X = np.concatenate(support_X, axis=0)
+		y = np.concatenate(support_y, axis=0)
+		support_set = pd.DataFrame(X)
+		support_set["y"] = y
+		prototyps = support_set.groupby("y").mean()
+		y_pred = [self.pn_prediction(prototyps, x) for x in self.features]
+		return y_pred
+
+	def run_active_learning(self, n_queries=100):
+		# initialization with K-means
+		support_X = []
+		support_y = []
+		self.scores = []
+		for index in range(n_queries):
+			# query process
+			query_idx = self.random_selection(self.X_pool)
+			support_X.append(self.X_pool[query_idx])
+			# labeling
+			support_y.append(self.y_pool[query_idx])
+			# training the model
+			y_pred = self.run_prototypical_network(support_X, support_y)
+			# evaluation
+			score = accuracy_score(self.y, y_pred)
+			self.scores.append(score)
+			# delete from the pool
+			self.X_pool = np.delete(self.X_pool, query_idx, axis=0)
+			self.y_pool = np.delete(self.y_pool, query_idx, axis=0)
 
 
 # %% main
 if __name__ == "__main__":
-	# pre-training process
-	HAR_data = load_dataset()
-	encoder = Encoder(HAR_data)
-	accuracy = encoder.train_model(epochs=3, verbose=0, plot_acc=True)
-	print(accuracy)
+	# # pre-training process
+	# HAR_data = load_dataset()
+	# encoder = Encoder(HAR_data)
+	# accuracy = encoder.train_model(epochs=3, verbose=0, plot_acc=True)
+	# print(accuracy)
 
 	# Prototypical Network
-	# model_path = "Encoder_models/15_02_2022__23_37_24"
+	# input with the same pre-processing (standardization)
+	HAR_data = load_dataset()
+	val_X = np.concatenate((HAR_data["trainX"], HAR_data["testX"]), axis=0)
+	# original label
+	label1 = load_file('./UCI HAR Dataset/train/y_train.txt')
+	label2 = load_file('./UCI HAR Dataset/test/y_test.txt')
+	val_label = np.concatenate((label1, label2), axis=0)
+	val_data = {"X": val_X, "y":val_label}
+	model_path = "Encoder_models/16_02_2022__00_42_06"
+	model = ActiveLearning(model_path, val_data)
+	model.run_active_learning()
+
 
 # %%
